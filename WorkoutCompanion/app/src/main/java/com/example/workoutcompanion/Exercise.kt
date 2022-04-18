d001bfe29e78a5928d6a9f7ac770ab2435eb6482
Eduard Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2022-04-18 08:03:03 +0300

    Workout Companion

---
+++ WorkoutCompanion/app/src/main/java/com/example/workoutcompanion/Exercise.kt
@@ -0,0 +1,9 @@
+package com.example.workoutcompanion
+
+import java.util.*
+
+class Exercise() {
+    val id_exercise: UUID = UUID.randomUUID()
+    var name: String = ""
+    var equipment: String = ""
+}
---
+++ WorkoutCompanion/app/src/main/java/com/example/workoutcompanion/ExerciseViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.workoutcompanion
+
+import androidx.lifecycle.ViewModel
+
+class ExerciseViewModel(): ViewModel() {
+    private val exercise = Exercise()
+
+    fun loadExercise()
+}
